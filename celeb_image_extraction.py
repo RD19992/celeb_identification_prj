30ee472d3d9f86e238cd03512d8902f51833935a
Renan Diniz <rios.diniz@gmail.com>
2025-11-30 16:24:56 -0300

    Adding file for image extraction

---
+++ celeb_image_extraction.py
@@ -0,0 +1,25 @@
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+# 1) Basic image transform
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+])
+
+# 2) Load CelebA
+celeba_train = datasets.CelebA(
+    root="data",              # folder where it will be stored
+    split="train",            # "train" | "valid" | "test" | "all"
+    target_type="attr",       # or "identity", "bbox", "landmarks"
+    transform=transform,
+    download=True,            # set False if already downloaded
+)
+
+loader = DataLoader(celeba_train, batch_size=64, shuffle=True)
+
+# Example: get one batch
+imgs, attrs = next(iter(loader))
+print(imgs.shape, attrs.shape)  # (64, 3, 128, 128), (64, 40)
