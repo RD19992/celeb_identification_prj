d61fb9c5e6af19ded71c2c274b056c642eea1694
Renan Diniz <rios.diniz@gmail.com>
2025-12-07 11:43:27 -0300

    Added identification check for downloaded images

--- data/celeb_image_processing.py
+++ data/celeb_image_processing.py
@@ -24,11 +24,27 @@ HERE = Path(__file__).resolve().parent
 ## Definindo raiz do local atual
 PROJECT_ROOT = HERE.parent
 
-## Apontando para local de download das imagens do dataset
-IMAGES_DIR = PROJECT_ROOT / "data" / "celeba" / "img_align_celeba"   # or "img_align_celeba", etc
+## Apontando para local de download das imagens do dataset e arquivo de identificação de pessoas
+DATA_DIR = PROJECT_ROOT / "data" / "celeba"
+IMAGES_DIR = DATA_DIR / "img_align_celeba"
+IDENTITY_FILE = DATA_DIR / "identity_CelebA.txt"
 
 ## Verificando presença do download das imagens
 print(IMAGES_DIR.exists())     # se correto retorna TRUE
 
 ## Contagem de arquivos
 print(len(os.listdir(IMAGES_DIR))) #Deve retornar 202599
+
+## Verificando se existe o mapeamento de imagem e pessoa (número de identificação)
+
+def load_identities(identity_path: Path = IDENTITY_FILE):
+    mapping = {}
+    with identity_path.open() as f:
+        for line in f:
+            fname, ident = line.strip().split()
+            mapping[fname] = int(ident)
+    return mapping
+
+identities = load_identities()
+print("Num identities:", len(identities))
+print("Sample:", list(identities.items())[:5])
