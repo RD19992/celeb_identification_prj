8b1d1f450b3c8332ddd70dd5523ad8b67210ddec
Renan Diniz <rios.diniz@gmail.com>
2025-12-21 15:17:06 -0300

    Created final file for MLP model training

--- data/data/celeb_MLP_model_training.py
+++ data/data/celeb_MLP_model_training.py
@@ -25,7 +25,7 @@ CONFIG = {
     "dataset_path": r"C:\Users\riosd\PycharmProjects\celeb_identification_prj\data\data\celeba_hog_128x128_o9.joblib",
 
     # seleção de classes (prototipagem)
-    "frac_classes": 1.00,  # ex.: 0.20 (20% das classes elegíveis)
+    "frac_classes": 0.01,  # ex.: 0.20 (20% das classes elegíveis)
     "seed_classes": 42,
     "min_amostras_por_classe": 25,
 
