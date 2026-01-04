2f0ec1fa5c5126b9fe3b5155466557926d2afb30
Renan Diniz <rios.diniz@gmail.com>
2026-01-04 10:14:11 -0300

    Created new files for CNN project

---
+++ data/data/cnn_identification_authorization/image_ingestion_processing.py
@@ -0,0 +1,8 @@
+## -*- coding: utf-8 -*-
+""""
+EACH_USP: SIN-5016 - Aprendizado de Máquina
+Laura Silva Pelicer
+Renan Rios Diniz
+
+Código para ingestão do dataset CELEB-A e preparação para processamento
+"""
